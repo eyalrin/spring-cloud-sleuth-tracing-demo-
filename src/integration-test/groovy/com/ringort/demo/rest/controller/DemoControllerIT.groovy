6886c0374f67985420d0eb3cb61bd615df50b420
Eyal Ringort <eyal.ringort@behalf.com>
2020-06-04 15:31:49 +0300

    Renamed test class

--- src/integration-test/groovy/com/ringort/demo/rest/controller/DemoControllerIT.groovy
+++ src/integration-test/groovy/com/ringort/demo/rest/controller/DemoControllerIT.groovy
@@ -1,6 +1,5 @@
 package com.ringort.demo.rest.controller
 
-
 import com.ringort.demo.SpringCloudSleuthTracingDemoApplication
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -8,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -19,7 +17,7 @@ import spock.lang.Specification
 @SpringBootTest(classes = SpringCloudSleuthTracingDemoApplication.class)
 @ActiveProfiles(profiles = ["test"])
 @AutoConfigureMockMvc
-class DemoControllerTest extends Specification {
+class DemoControllerIT extends Specification {
 
     @Autowired
     WebApplicationContext webApplicationContext
